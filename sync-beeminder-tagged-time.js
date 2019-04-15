9d0a0f1e8cd33d84ffd3a2194dd187f5e3da715b
Colin Z. Robertson <czr@rtnl.org.uk>
2019-04-15 16:40:27 +0100

    Remove non-OO interface

--- sync-beeminder-tagged-time.js
+++ sync-beeminder-tagged-time.js
@@ -2,40 +2,6 @@
 
 const moment = require('moment')
 
-function sortAndFilterDatapoints(datapoints, since) {
-  var filtered = datapoints.filter(datapoint => {
-    return datapoint.comment.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)
-  }).filter(datapoint => {
-    var m = moment(datapoint.comment)
-    return m.isSameOrAfter(since)
-  })
-  var sorted = filtered
-  sorted.sort((a, b) => {
-    if (a.comment < b.comment) {
-      return -1
-    }
-    if (a.comment > b.comment) {
-      return 1
-    }
-    return 0
-  })
-  return sorted
-}
-
-function sortEvents(events) {
-  var sorted = events
-  sorted.sort((a, b) => {
-    if (a.startDate < b.startDate) {
-      return -1
-    }
-    if (a.startDate > b.startDate) {
-      return 1
-    }
-    return 0
-  })
-  return sorted
-}
-
 function eventDuration(event) {
   var startTime = moment(event.startDate)
   var endTime = moment(event.endDate)
@@ -43,100 +9,6 @@ function eventDuration(event) {
   return moment.duration(endTime.diff(startTime)).asMinutes()
 }
 
-function calcSyncActions(events, datapoints) {
-  events = events.slice(0)
-  datapoints = datapoints.slice(0)
-
-  var actions = {insert: [], delete: [], update: []}
-
-  var currEvent = events.shift()
-  var currDatapoint = datapoints.shift()
-
-  var counter = 0;
-  while (currEvent && currDatapoint) {
-    if (currEvent.startDate.toISOString() === currDatapoint.comment) {
-      if (eventDuration(currEvent) !== currDatapoint.value) {
-        actions.update.push({
-          id: currDatapoint.id,
-          value: eventDuration(currEvent),
-          comment: currDatapoint.comment,
-        })
-      }
-      currEvent = events.shift()
-      currDatapoint = datapoints.shift()
-    }
-    else if (currEvent.startDate.toISOString() > currDatapoint.comment) {
-      actions.delete.push(currDatapoint.id)
-      currDatapoint = datapoints.shift()
-    }
-    else if (currEvent.startDate.toISOString() < currDatapoint.comment) {
-      actions.insert.push({
-        comment: currEvent.startDate.toISOString(),
-        value: eventDuration(currEvent),
-        daystamp: moment(currEvent.startDate).format('YYYYMMDD'),
-      })
-      currEvent = events.shift()
-    }
-  }
-
-  while (currEvent) {
-    actions.insert.push({
-      comment: currEvent.startDate.toISOString(),
-      value: eventDuration(currEvent),
-      daystamp: moment(currEvent.startDate).format('YYYYMMDD'),
-    })
-    currEvent = events.shift()
-  }
-
-  while (currDatapoint) {
-    actions.delete.push(currDatapoint.id)
-    currDatapoint = datapoints.shift()
-  }
-
-  return actions
-}
-
-function applyActions(actions, goal) {
-  var promises = []
-
-  for (let insertDatapoint of actions.insert) {
-    promises.push(
-      goal.createDatapoint(insertDatapoint)
-        .then((result) => {
-          console.log('Created datapoint: ' + result)
-        })
-        .catch((error) => {
-          console.log('Failed to create datapoint: ' + error)
-        })
-    )
-  }
-
-  for (let updateDatapoint of actions.update) {
-    promises.push(
-      goal.updateDatapoint(updateDatapoint)
-        .then((result) => {
-          console.log('Updated datapoint: ' + updateDatapoint)
-        })
-        .catch((error) => {
-          console.log('Failed to update datapoint: ' + error)
-        })
-    )
-  }
-
-  for (let deleteDatapoint of actions.delete) {
-    promises.push(
-      goal.deleteDatapoint(deleteDatapoint)
-        .then((result) => {
-          console.log('Deleted datapoint: ' + deleteDatapoint)
-        })
-        .catch((error) => {
-          console.log('Failed to delete datapoint: ' + error)
-        })
-    )
-  }
-
-  return Promise.all(promises)
-}
 
 class BeeminderTimeSync {
   constructor(goal, events, startDate) {
@@ -146,23 +18,142 @@ class BeeminderTimeSync {
   }
 
   sortedEvents() {
-    return sortEvents(this.events)
+    var sorted = this.events
+    sorted.sort((a, b) => {
+      if (a.startDate < b.startDate) {
+        return -1
+      }
+      if (a.startDate > b.startDate) {
+        return 1
+      }
+      return 0
+    })
+    return sorted
   }
 
   async datapoints() {
-    var datapoints = await this.goal.datapoints()
-    return sortAndFilterDatapoints(datapoints, this.startDate)
+    var allDatapoints = await this.goal.datapoints()
+    var since = this.startDate
+
+    var filtered = allDatapoints.filter(datapoint => {
+      return datapoint.comment.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)
+    }).filter(datapoint => {
+      var m = moment(datapoint.comment)
+      return m.isSameOrAfter(since)
+    })
+
+    var sorted = filtered
+    sorted.sort((a, b) => {
+      if (a.comment < b.comment) {
+        return -1
+      }
+      if (a.comment > b.comment) {
+        return 1
+      }
+      return 0
+    })
+
+    return sorted
   }
 
   async actions() {
+    var events = this.sortedEvents().slice(0)
     var datapoints = await this.datapoints()
-    return calcSyncActions(this.sortedEvents(), datapoints)
+    var datapoints = datapoints.slice(0)
+
+    var actions = {insert: [], delete: [], update: []}
+
+    var currEvent = events.shift()
+    var currDatapoint = datapoints.shift()
+
+    var counter = 0;
+    while (currEvent && currDatapoint) {
+      if (currEvent.startDate.toISOString() === currDatapoint.comment) {
+        if (eventDuration(currEvent) !== currDatapoint.value) {
+          actions.update.push({
+            id: currDatapoint.id,
+            value: eventDuration(currEvent),
+            comment: currDatapoint.comment,
+          })
+        }
+        currEvent = events.shift()
+        currDatapoint = datapoints.shift()
+      }
+      else if (currEvent.startDate.toISOString() > currDatapoint.comment) {
+        actions.delete.push(currDatapoint.id)
+        currDatapoint = datapoints.shift()
+      }
+      else if (currEvent.startDate.toISOString() < currDatapoint.comment) {
+        actions.insert.push({
+          comment: currEvent.startDate.toISOString(),
+          value: eventDuration(currEvent),
+          daystamp: moment(currEvent.startDate).format('YYYYMMDD'),
+        })
+        currEvent = events.shift()
+      }
+    }
+
+    while (currEvent) {
+      actions.insert.push({
+        comment: currEvent.startDate.toISOString(),
+        value: eventDuration(currEvent),
+        daystamp: moment(currEvent.startDate).format('YYYYMMDD'),
+      })
+      currEvent = events.shift()
+    }
+
+    while (currDatapoint) {
+      actions.delete.push(currDatapoint.id)
+      currDatapoint = datapoints.shift()
+    }
+
+    return actions
   }
 
   async apply() {
     var actions = await this.actions()
-    return applyActions(actions, this.goal)
+    var goal = this.goal
+
+    var promises = []
+
+    for (let insertDatapoint of actions.insert) {
+      promises.push(
+        goal.createDatapoint(insertDatapoint)
+          .then((result) => {
+            console.log('Created datapoint: ' + result)
+          })
+          .catch((error) => {
+            console.log('Failed to create datapoint: ' + error)
+          })
+      )
+    }
+
+    for (let updateDatapoint of actions.update) {
+      promises.push(
+        goal.updateDatapoint(updateDatapoint)
+          .then((result) => {
+            console.log('Updated datapoint: ' + updateDatapoint)
+          })
+          .catch((error) => {
+            console.log('Failed to update datapoint: ' + error)
+          })
+      )
+    }
+
+    for (let deleteDatapoint of actions.delete) {
+      promises.push(
+        goal.deleteDatapoint(deleteDatapoint)
+          .then((result) => {
+            console.log('Deleted datapoint: ' + deleteDatapoint)
+          })
+          .catch((error) => {
+            console.log('Failed to delete datapoint: ' + error)
+          })
+      )
+    }
+
+    return Promise.all(promises)
   }
 }
 
-module.exports = { calcSyncActions, sortEvents, sortAndFilterDatapoints, applyActions, BeeminderTimeSync };
+module.exports = { BeeminderTimeSync };
--- sync-beeminder-tagged-time.test.js
+++ sync-beeminder-tagged-time.test.js
@@ -4,40 +4,6 @@ const moment = require('moment')
 
 const sync = require('./sync-beeminder-tagged-time')
 
-var datapoints = [
-  {"id":"1", "timestamp":1234567890, "daystamp":"20190223", "value":6, "comment":"2019-02-23T01:00:00.000Z", "updated_at":123, "requestid":"a"},
-  {"id":"2", "timestamp":1234567890, "daystamp":"20190221", "value":7, "comment":"2019-02-21T01:00:00.000Z", "updated_at":123, "requestid":"b"},
-  {"id":"3", "timestamp":1234567891, "daystamp":"20190222", "value":8, "comment":"2019-02-22T01:00:00.000Z", "updated_at":123, "requestid":"c"},
-  {"id":"4", "timestamp":1234567891, "daystamp":"20190222", "value":9, "comment":"", "updated_at":123, "requestid":"d"},
-  {"id":"5", "timestamp":1234567890, "daystamp":"20190220", "value":0, "comment":"2019-02-20T01:00:00.000Z", "updated_at":123, "requestid":"e"},
-]
-
-test('sorted, filtered datapoints', () => {
-  expect(sync.sortAndFilterDatapoints(datapoints, moment('2019-02-21'))).toMatchObject([
-    {"id":"2", "timestamp":1234567890, "daystamp":"20190221", "value":7, "comment":"2019-02-21T01:00:00.000Z", "updated_at":123, "requestid":"b"},
-    {"id":"3", "timestamp":1234567891, "daystamp":"20190222", "value":8, "comment":"2019-02-22T01:00:00.000Z", "updated_at":123, "requestid":"c"},
-    {"id":"1", "timestamp":1234567890, "daystamp":"20190223", "value":6, "comment":"2019-02-23T01:00:00.000Z", "updated_at":123, "requestid":"a"},
-  ])
-})
-
-var events = [
-  {
-    startDate: new Date('2019-02-21T01:00:00'),
-    endDate: new Date('2019-02-21T02:00:00'),
-    summary: 'One #music',
-  },
-  {
-    startDate: new Date('2019-02-24T01:00:00'),
-    endDate: new Date('2019-02-24T02:00:00'),
-    summary: 'Three #music',
-  },
-  {
-    startDate: new Date('2019-02-22T01:00:00'),
-    endDate: new Date('2019-02-22T02:00:00'),
-    summary: 'Two #music',
-  },
-]
-
 function createOkPromise(data) {
   return new Promise((ok, err) => { ok(data) })
 }
@@ -54,9 +20,17 @@ function mockGoal(datapoints) {
 }
 
 test('sorted, filtered datapoints', async () => {
+  var datapoints = [
+    {"id":"1", "timestamp":1234567890, "daystamp":"20190223", "value":6, "comment":"2019-02-23T01:00:00.000Z", "updated_at":123, "requestid":"a"},
+    {"id":"2", "timestamp":1234567890, "daystamp":"20190221", "value":7, "comment":"2019-02-21T01:00:00.000Z", "updated_at":123, "requestid":"b"},
+    {"id":"3", "timestamp":1234567891, "daystamp":"20190222", "value":8, "comment":"2019-02-22T01:00:00.000Z", "updated_at":123, "requestid":"c"},
+    {"id":"4", "timestamp":1234567891, "daystamp":"20190222", "value":9, "comment":"", "updated_at":123, "requestid":"d"},
+    {"id":"5", "timestamp":1234567890, "daystamp":"20190220", "value":0, "comment":"2019-02-20T01:00:00.000Z", "updated_at":123, "requestid":"e"},
+  ]
+
   const syncer = new sync.BeeminderTimeSync(
     mockGoal(datapoints),
-    events,
+    [],
     moment('2019-02-21'),
   )
 
@@ -70,28 +44,24 @@ test('sorted, filtered datapoints', async () => {
 })
 
 test('sorted events', () => {
-  var sorted = sync.sortEvents(events)
-
-  expect(sorted).toMatchObject([
+  var events = [
     {
       startDate: new Date('2019-02-21T01:00:00'),
       endDate: new Date('2019-02-21T02:00:00'),
       summary: 'One #music',
     },
     {
-      startDate: new Date('2019-02-22T01:00:00'),
-      endDate: new Date('2019-02-22T02:00:00'),
-      summary: 'Two #music',
-    },
-    {
       startDate: new Date('2019-02-24T01:00:00'),
       endDate: new Date('2019-02-24T02:00:00'),
       summary: 'Three #music',
     },
-  ])
-})
+    {
+      startDate: new Date('2019-02-22T01:00:00'),
+      endDate: new Date('2019-02-22T02:00:00'),
+      summary: 'Two #music',
+    },
+  ]
 
-test('sorted events', () => {
   const syncer = new sync.BeeminderTimeSync(
     mockGoal(),
     events,
@@ -116,94 +86,6 @@ test('sorted events', () => {
       summary: 'Three #music',
     },
   ])
-})
-
-describe('calcSyncActions', () => {
-  test('no data', () => {
-    var events = []
-    var datapoints = []
-
-    var actions = sync.calcSyncActions(events, datapoints)
-
-    expect(actions).toEqual({insert: [], delete: [], update: []})
-  })
-
-  test('one event', () => {
-    var events = [
-      {
-        startDate: new Date('2019-02-21T01:00:00'),
-        endDate: new Date('2019-02-21T02:00:00'),
-        summary: 'One #music',
-      }
-    ]
-    var datapoints = []
-
-    var actions = sync.calcSyncActions(events, datapoints)
-
-    expect(actions).toEqual({
-      insert: [
-        {
-          comment: '2019-02-21T01:00:00.000Z',
-          value: 60,
-          daystamp: '20190221',
-        },
-      ],
-      delete: [],
-      update: [],
-    })
-  })
-
-  test('one datapoint', () => {
-    var events = []
-    var datapoints = [
-      {
-        id: 101,
-        comment: '2019-02-21T01:00:00.000Z',
-        value: 60,
-      },
-    ]
-
-    var actions = sync.calcSyncActions(events, datapoints)
-
-    expect(actions).toEqual({
-      insert: [],
-      delete: [
-        101,
-      ],
-      update: [],
-    })
-  })
-
-  test('update', () => {
-    var events = [
-      {
-        startDate: new Date('2019-02-21T01:00:00'),
-        endDate: new Date('2019-02-21T02:00:00'),
-        summary: 'One #music',
-      }
-    ]
-    var datapoints = [
-      {
-        id: 101,
-        comment: '2019-02-21T01:00:00.000Z',
-        value: 30,
-      },
-    ]
-
-    var actions = sync.calcSyncActions(events, datapoints)
-
-    expect(actions).toEqual({
-      insert: [],
-      delete: [],
-      update: [
-        {
-          id: 101,
-          comment: '2019-02-21T01:00:00.000Z',
-          value: 60,
-        }
-      ],
-    })
-  })
 })
 
 describe('calculate sync actions', () => {
@@ -315,92 +197,7 @@ describe('calculate sync actions', () => {
   })
 })
 
-describe('applyActions', () => {
-  test('no actions', () => {
-    var goal = mockGoal()
-    var actions = {insert: [], update: [], delete: []}
-
-    sync.applyActions(actions, goal)
-
-    expect(goal.datapoints).not.toHaveBeenCalled()
-    expect(goal.createDatapoint).not.toHaveBeenCalled()
-    expect(goal.deleteDatapoint).not.toHaveBeenCalled()
-    expect(goal.updateDatapoint).not.toHaveBeenCalled()
-  })
-
-  test('insert', () => {
-    var goal = mockGoal()
-    var actions = {
-      insert: [
-        {
-          comment: '2019-02-21T01:00:00.000Z',
-          value: 60,
-        },
-      ],
-      update: [],
-      delete: [],
-    }
-
-    sync.applyActions(actions, goal)
-
-    expect(goal.datapoints).not.toHaveBeenCalled()
-    expect(goal.createDatapoint).toHaveBeenCalledWith(
-      {
-        comment: '2019-02-21T01:00:00.000Z',
-        value: 60,
-      }
-    )
-    expect(goal.deleteDatapoint).not.toHaveBeenCalled()
-    expect(goal.updateDatapoint).not.toHaveBeenCalled()
-  })
-
-  test('update', () => {
-    var goal = mockGoal()
-    var actions = {
-      insert: [],
-      update: [
-        {
-          id: 101,
-          comment: '2019-02-21T01:00:00.000Z',
-          value: 60,
-        },
-      ],
-      delete: [],
-    }
-
-    sync.applyActions(actions, goal)
-
-    expect(goal.datapoints).not.toHaveBeenCalled()
-    expect(goal.createDatapoint).not.toHaveBeenCalled()
-    expect(goal.updateDatapoint).toHaveBeenCalledWith(
-      {
-        id: 101,
-        comment: '2019-02-21T01:00:00.000Z',
-        value: 60,
-      }
-    )
-    expect(goal.deleteDatapoint).not.toHaveBeenCalled()
-  })
-
-  test('insert', () => {
-    var goal = mockGoal()
-    var actions = {
-      insert: [],
-      update: [],
-      delete: [101],
-    }
-
-    sync.applyActions(actions, goal)
-
-    expect(goal.datapoints).not.toHaveBeenCalled()
-    expect(goal.createDatapoint).not.toHaveBeenCalled()
-    expect(goal.updateDatapoint).not.toHaveBeenCalled()
-    expect(goal.deleteDatapoint).toHaveBeenCalledWith(101)
-  })
-
-})
-
-describe('applyActions', () => {
+describe('apply', () => {
   test('no actions', async () => {
     var goal = mockGoal()
     var actions = {insert: [], update: [], delete: []}
